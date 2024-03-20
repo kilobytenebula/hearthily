a79eb88df062a8afcb860cff896ac09fc9c04625
Thejani Kulathunga <128416425+thejk10@users.noreply.github.com>
2024-03-21 01:01:50 +0530

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+
+
+const PORT = process.env.PORT || 8070;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL,{
+    //useCreateIndex: true,
+    //useNewUrlParser: true,
+    //useUnifiedTopology: true,
+    //useFindAndModify: true
+});
+
+const connection = mongoose.connection;
+connection.once("open",() =>{
+    console.log("Mongodb connection success!");
+})
+
+const baseRouter = require("./routes/bases.js");
+app.use("/base",baseRouter);
+
+const portionRouter = require("./routes/portions.js");
+app.use("/portion",portionRouter);
+
+const ordereRouter = require("./routes/orders.js");
+app.use("/order",ordereRouter);
+
+app.listen(PORT, () =>{
+    console.log(`Server is up and running on port number: ${PORT}`)
+})
