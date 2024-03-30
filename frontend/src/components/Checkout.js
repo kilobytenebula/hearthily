9400c6e6422c89a69984974f5292c3c30b61fd9f
Thejani Kulathunga <thejanikulathunga10@gmail.com>
2024-03-30 12:26:05 +0530

    Update Checkout.js

--- frontend/src/components/Checkout.js
+++ frontend/src/components/Checkout.js
@@ -1,5 +1,26 @@
-import React from "react";
+import React, {useEffect} from "react";
+import { useParams } from "react-router-dom";
 
 export default function Checkout(){
-    
+    const {baseName, basePrice, selectedPortions, total} = useParams();
+    const parsedPortions = JSON.parse(selectedPortions);
+
+    return(
+        <div className="container">
+            <div className="content">
+                <h4>Main Dish: {baseName} </h4>
+                <h4>Price: {basePrice} </h4>
+                <h4>Portions: </h4>
+                <ul>
+                    {parsedPortions.map((portion, index) => (
+                    <li key={index}>
+                        <p>Name: {portion.name}</p>
+                        <p>Price: {portion.price}</p>
+                    </li>
+                    ))}
+                </ul>
+                <h4>Total: {total}</h4>
+            </div>
+        </div>
+    )
 }
