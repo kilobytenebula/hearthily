74ff1186364fb5655bd3839fdafc72ca83816f96
Thejani Kulathunga <thejanikulathunga10@gmail.com>
2024-04-22 17:03:46 +0530

    improved search function

--- frontend/src/components/AllBases.js
+++ frontend/src/components/AllBases.js
@@ -4,86 +4,104 @@ import { Link } from 'react-router-dom';
 import '../allBases.css'
 
 
-
 export default function AllBases(){
     const [bases, setBases] = useState([]);
     const [selectedCategory, setSelectedCategory] = useState("Sri Lankan Dishes");
+    const [records, setRecords] = useState([]);
+    const [riceBases, setRiceBases] = useState([]);
+    const [otherBases, setOtherBases] = useState([]);
+    const [filteredBases, setFilteredBases] = useState([]);
+    const [defaultBases, setDefaultBases] = useState([]);
 
     useEffect(()=>{
         function getBases(){
             axios.get("http://localhost:8070/base/").then((res)=>{
                 setBases(res.data);
+                setRecords(res.data);
+                setDefaultBases(res.data.filter(base => base.category === selectedCategory));
+                setRiceBases(res.data.filter(base => base.category === selectedCategory && base.base_type === "Rice"));
+                setOtherBases(res.data.filter(base => base.category === selectedCategory && base.base_type === "Others"));
+                setFilteredBases(res.data.filter(base => base.category === selectedCategory));
                 //console.log(res.data);
             }).catch((err)=>{
                 alert(err.message);
             })
         }
         getBases();
-    },[])
+    },[selectedCategory])
 
-    const filteredBases = bases.filter(
-        (base) => base.category === selectedCategory
-    );
-
-    const riceBases = selectedCategory === "Sri Lankan Dishes" ? filteredBases.filter((base) => base.base_type === "Rice") : [];
-    const otherBases = selectedCategory === "Sri Lankan Dishes" ? filteredBases.filter((base) => base.base_type === "Others") : filteredBases;
+    const Filter =(event) =>{
+        const searchValue = event.target.value.toLowerCase();
+        if (searchValue === "") {
+            setRecords(defaultBases);
+            setRiceBases(defaultBases.filter(base => base.base_type === "Rice"));
+            setOtherBases(defaultBases.filter(base => base.base_type === "Others"));
+            setFilteredBases(defaultBases);
+        } else {
+            const filteredRecords = defaultBases.filter(f => 
+                f.base_name.toLowerCase().includes(searchValue)
+            );
+            setRecords(filteredRecords);
+            setRiceBases(filteredRecords.filter(base => base.base_type === "Rice"));
+            setOtherBases(filteredRecords.filter(base => base.base_type === "Others"));
+            setFilteredBases(filteredRecords);
+        }
+    }
 
     return(
         <div className="container1">
             <div className="headerContainer">
-             <div className="heading">What are you craving today?</div>
-             <ul>
-                <li onClick={()=> setSelectedCategory("Sri Lankan Dishes")}>Sri Lankan Dishes</li>
-                <li onClick={()=> setSelectedCategory("Bakery Items")}>Bakery Items</li>
-                <li onClick={()=> setSelectedCategory("Beverages")}>Beverages</li>
-             </ul>
+                <div className="heading">What are you craving today?</div>
+                <div className="search-container">
+                    <input type="text" className="search" placeholder='Search..' onChange={Filter} />
+                </div>
+                <ul>
+                    <li onClick={()=> setSelectedCategory("Sri Lankan Dishes")}>Sri Lankan Dishes</li>
+                    <li onClick={()=> setSelectedCategory("Bakery Items")}>Bakery Items</li>
+                    <li onClick={()=> setSelectedCategory("Beverages")}>Beverages</li>
+                </ul>
             </div>
             
-            {/* <div className="base-category">Rice</div>  */}
-            
             {selectedCategory === "Sri Lankan Dishes" && (
-              <div>
-                <div className="base-category">Rice</div> 
-                <div className="bases-list">
-                    {riceBases.map((base) => (
-                    <div key={base._id} className="base_item">
-                        <h2>{base.base_name}</h2>
-                        <p>Reg price: {base.reg_price}LKR</p>
-                        <p>Full price: {base.full_price}LKR</p>
-                        <Link to={`/all-portions/${base._id}`}>Portions</Link>
+                <div>
+                    <div className="base-category">Rice</div> 
+                    <div className="bases-list">
+                        {riceBases.map((base) => (
+                            <div key={base._id} className="base_item">
+                                <h2>{base.base_name}</h2>
+                                <p>Reg price: {base.reg_price}LKR</p>
+                                <p>Full price: {base.full_price}LKR</p>
+                                <Link to={`/all-portions/${base._id}`}>Portions</Link>
+                            </div>
+                        ))}
                     </div>
-                    ))}
-                </div>
-                <div className="base-category">Others</div>
-                <div className="bases-list">
-                    {otherBases.map((base) => (
-                    <div key={base._id} className="base_item">
-                        <h2>{base.base_name}</h2>
-                        <p>Reg price: {base.reg_price}LKR</p>
-                        <p>Full price: {base.full_price}LKR</p>
-                        <Link to={`/all-portions/${base._id}`}>Portions</Link>
+                    <div className="base-category">Others</div>
+                    <div className="bases-list">
+                        {otherBases.map((base) => (
+                            <div key={base._id} className="base_item">
+                                <h2>{base.base_name}</h2>
+                                <p>Reg price: {base.reg_price}LKR</p>
+                                <p>Full price: {base.full_price}LKR</p>
+                                <Link to={`/all-portions/${base._id}`}>Portions</Link>
+                            </div>
+                        ))}
                     </div>
-                    ))}
                 </div>
-              </div>
             )}
 
             {selectedCategory !== "Sri Lankan Dishes" && (
-              <div className="bases-list">
-                {filteredBases.map((base) => (
-                    <div key={base._id} className="base_item">
-                        <h2>{base.base_name}</h2>
-                        <p>Reg price: {base.reg_price}LKR</p>
-                        <p>Full price: {base.full_price}LKR</p>
-                        <Link to={`/all-portions/${base._id}`}>Portions</Link>
-                    </div>
-                ))}
-              </div>
+                <div className="bases-list">
+                    {filteredBases.map((base) => (
+                        <div key={base._id} className="base_item">
+                            <h2>{base.base_name}</h2>
+                            <p>Reg price: {base.reg_price}LKR</p>
+                            <p>Full price: {base.full_price}LKR</p>
+                            <Link to={`/all-portions/${base._id}`}>Portions</Link>
+                        </div>
+                    ))}
+                </div>
             )}
-
-            
-
-
         </div>
     )
 }
+
