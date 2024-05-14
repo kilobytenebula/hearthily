6936c04f67b4c28cf9296ce4b9de1508527057ea
Savindu Sihilel <lk.ssihilel@gmail.com>
2024-05-14 15:27:42 +0530

    Fixed Delivery Summary, improved navigation

--- frontend/src/components/navBar.js
+++ frontend/src/components/navBar.js
@@ -37,8 +37,7 @@ const NavBar = () => {
                 {userRole === 'admin' && <li><Link to="/refunds"><div className='navItem'><img src={refund} alt="Refunds" title="Refunds"></img></div></Link></li>}
                 {userRole === 'admin' && <li><Link to="/admin-panel-order-history"><div className='navItem'><img src={orderHistory} alt="Order History" title='Order History' /></div></Link></li>}
                 {userRole === 'admin' && <li><Link to="/paymentReports"><div className='navItem'><img src={paymentReports} alt="Payment Reports" title="Payment Reports"></img></div></Link></li>}
-                {/* {userRole === 'admin' && <li><Link to="/job-summary"><div className='navItem'><img src={delivery} alt="Job Summary" title='Job Summary' /></div></Link></li>} */}
-                {userRole === 'admin' && <li><Link to="/drivers"><div className='navItem'><img src={drivers} alt="Driver Perfomance" title='Driver Perfomance' /></div></Link></li>}
+                {userRole === 'admin' && <li><Link to="/delivery-summary"><div className='navItem'><img src={delivery} alt="Job Summary" title='Job Summary' /></div></Link></li>}
                 {userRole === 'driver' && <li><Link to="/delivery"><div className='navItem'><img src={delivery} alt="Available Jobs" title='Available Jobs' /></div></Link></li>}
                 {userRole === 'driver' && <li><Link to="/job-history"><div className='navItem'><img src={jobHistory} alt="Job History" title='Job History' /></div></Link></li>}
                 {userRole === 'inventory' && <li><Link to="/display"><div className='navItem'><img src={box} alt="Suppliers" title='Suppliers' /></div></Link></li>}
